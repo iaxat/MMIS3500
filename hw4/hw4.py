144bf78f743c0d4cbc85ffa7035544b4c6cde216
Kumar Akshat <55215422+iaxat@users.noreply.github.com>
2020-10-19 05:07:28 -0600

    up

--- hw4/hw4.py
+++ hw4/hw4.py
@@ -1,3 +1,13 @@
 # The following is the homework 4 for course MIS3500
 # This assignment is Home Work 4
 
+def file_read():
+    file = open("AAPL.txt", "r")
+    lines = file.readlines()
+    for line in lines:
+        price = float(line)
+        print(price)
+
+
+# The main function goes here
+file_read()
