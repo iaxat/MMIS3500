59ff6344a67feaa113c1802d5bebd7e4a1ebe4a6
Kumar Akshat <55215422+iaxat@users.noreply.github.com>
2020-10-19 19:00:37 -0600

    up

--- hw4/mv4.py
+++ hw4/mv4.py
@@ -1,1 +1,8 @@
-print("hello")
+file = open("AAPL.txt", "r")
+lines = file.readlines()
+prices = []
+for line in lines:
+    price = float(line)
+    prices.append(price)
+    print(price)
+print(prices)
