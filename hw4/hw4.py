b5a66d4ef6ec40c3d5a4f028dca820cb135661d2
Kumar Akshat <55215422+iaxat@users.noreply.github.com>
2020-10-19 19:41:46 -0600

    up

--- hw4/hw4.py
+++ hw4/hw4.py
@@ -29,9 +29,13 @@ def file_read():
                 current_price = prices[i]
                 moving_average = (prices[i-1] + prices[i-2] + prices[i-3] + prices[i-4] + prices[i-5]) / 5
                 print("The Moving Average for last 5 days is", moving_average)
+                
                 if (current_price < .95*moving_average):
                     buy = prices[i]
-                    first_buy
+                    print(buy)
+
+
+
             i += 1
 
 
