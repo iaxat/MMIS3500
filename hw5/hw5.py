b8c1660307d6206b9603db227bcd9d349efd390f
Kumar Akshat <55215422+iaxat@users.noreply.github.com>
2020-11-16 22:26:23 -0700

    up

--- hw5/hw5.py
+++ hw5/hw5.py
@@ -8,32 +8,35 @@ mean_reversion_dict = {}
 simple_average_dict = {}
 
 # The Mean Reversion Strategy Code Below
-def meanReversionStrategy(prices,file):
-    add = 0 # variable for adding total
-    counter = 0 # counter to understand how many counts are there
+
+
+def meanReversionStrategy(prices, file):
+    add = 0  # variable for adding total
+    counter = 0  # counter to understand how many counts are there
     buy = 0
     iterative_profit = 0
     total_profit = 0
     first_buy = 0
 
-        # Getting back to Moving Average
+    # Getting back to Moving Average
     i = 0
     for price in prices:
         add += price
         counter += 1
         if i >= 5:
             current_price = price
-            moving_average = (prices[i-1] + prices[i-2] + prices[i-3] + prices[i-4] + prices[i-5]) / 5
+            moving_average = (prices[i-1] + prices[i-2] +
+                              prices[i-3] + prices[i-4] + prices[i-5]) / 5
             # print("The Moving Average for last 5 days is", moving_average)
-            
+
             if (current_price < 0.95*moving_average) and buy == 0:
                 buy = current_price
-                print("Buying the Stock",buy)
+                print("Buying the Stock", buy)
                 if first_buy == 0:
                     first_buy = buy
                     print("The first buy is at: ", first_buy)
 
-            elif (current_price > 1.05*moving_average) and buy!=0:
+            elif (current_price > 1.05*moving_average) and buy != 0:
                 print("Selling stock at: ", current_price)
                 iterative_profit = current_price - buy
                 buy = 0
@@ -41,8 +44,7 @@ def meanReversionStrategy(prices,file):
                 total_profit += iterative_profit
                 print("")
 
-
-        i += 1 # Iteration changes the loop process
+        i += 1  # Iteration changes the loop process
 
     # Now processing the profits
     print("-----------------------MEAN REVERSION total profits earned from the first buy----------------------")
@@ -51,12 +53,12 @@ def meanReversionStrategy(prices,file):
     print("The total profit percentage is: ", final_profit_percent)
     print("")
 
-    mean_reversion_dict [file] = {'total profit':total_profit,'profit percent':final_profit_percent}
+    mean_reversion_dict[file] = {
+        'total profit': total_profit, 'profit percent': final_profit_percent}
 
     # Unrelated but was in the class video so added
     total_avg = add/counter
     print("Total Average for price for the whole list is: ", total_avg)
-
 
 
 # # The main function goes here
@@ -64,14 +66,13 @@ def meanReversionStrategy(prices,file):
 
 
 # Function for simple moving average
-def simpleMovingAverage(prices,file):
+def simpleMovingAverage(prices, file):
     add = 0  # variable for adding total
     counter = 0  # counter to understand how many counts are there
     buy = 0
     iterative_profit = 0
     total_profit = 0
     first_buy = 0
-
 
     # Getting back to Moving Average
     i = 0
@@ -112,14 +113,21 @@ def simpleMovingAverage(prices,file):
     total_avg = add/counter
     print("Total Average for price for the whole list is: ", total_avg)
 
-    simple_average_dict[file] = {'total profit': total_profit, 'profit percent': final_profit_percent}
+    simple_average_dict[file] = {
+        'total profit': total_profit, 'profit percent': final_profit_percent}
 
 
 # function to safe results to dict and then to json file
-def dict_to_json(dict1,dict2):
-    # print(dict_)
-    # json_object = json.dumps(dict_)
-    # print(json_object)
+def dict_to_json(dict1, dict2):
+    print(dict1)
+    print()
+    json_object1 = json.dumps(dict1)
+    print(json_object1)
+    print()
+    print(dict2)
+    json_object2 = json.dumps(dict2)
+    print((json_object2))
+    print()
     with open("mean_reversion.json", "w") as outfile:
         json.dump(dict1, outfile)
     with open("simple_average.json", "w") as outfile:
@@ -128,18 +136,20 @@ def dict_to_json(dict1,dict2):
 
 def file_read():
     # Function for mean reversion strategy
-    file_names_list = ['AAPL.txt','CSCO.txt','FB.txt','GOOGL.txt','JPM.txt','MSFT.txt','TMUS.txt','TSLA.txt','TTM.txt','XOM.txt']
+    file_names_list = ['AAPL.txt', 'CSCO.txt', 'FB.txt', 'GOOGL.txt',
+                       'JPM.txt', 'MSFT.txt', 'TMUS.txt', 'TSLA.txt', 'TTM.txt', 'XOM.txt']
     for file_name in file_names_list:
+
         price_list = []
-        file = open(file_name,"r")
+        file = open(file_name, "r")
         lines = file.readlines()
         for line in lines:
             price = float(line)
             price_list.append(price)
-    
-        meanReversionStrategy(price_list,file_name)
-        simpleMovingAverage(price_list,file_name)
-    
+
+        meanReversionStrategy(price_list, file_name)
+        simpleMovingAverage(price_list, file_name)
+
     dict_to_json(mean_reversion_dict, simple_average_dict)
 
 
