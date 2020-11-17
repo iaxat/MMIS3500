0fa0ef89578fe034a27e61cec1ded475a3087dcd
Kumar Akshat <55215422+iaxat@users.noreply.github.com>
2020-11-16 17:38:00 -0700

    up

--- hw5/hw5.py
+++ hw5/hw5.py
@@ -3,15 +3,15 @@
 
 # Function for mean reversion strategy
 def meanReversionStrategy():
-
+    print('Mean Reversion Technique')
 
 # Function for simple moving average
 def simpleMovingAverage():
-
+    print('Simple Moving Average')
 
 # function to safe results to dict and then to json file
 def saveResults():
-
+    print('Save Result')
 
 
 
