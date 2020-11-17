6437f8f576fa0ef5a950e5eaca50ce312fe57d64
Kumar Akshat <55215422+iaxat@users.noreply.github.com>
2020-11-16 17:37:01 -0700

    up

--- hw5/hw5.py
+++ hw5/hw5.py
@@ -1,11 +1,15 @@
 # This project is about HomeWork 5
 
+
+# Function for mean reversion strategy
 def meanReversionStrategy():
 
 
+# Function for simple moving average
 def simpleMovingAverage():
 
 
+# function to safe results to dict and then to json file
 def saveResults():
 
 
