5ee35ad3c5233b23e6078d2fb7738f65fdc7b0b3
Kumar Akshat <55215422+iaxat@users.noreply.github.com>
2020-12-15 15:51:26 -0700

    up

--- activities/week12.py
+++ activities/week12.py
@@ -4,3 +4,13 @@
 # Class and Objects
 # Section 10.1 and 10.2
 
+person = {}
+person["name"] = "andy"
+person["age"] = 39
+person["height"] = 75
+person["grades"] = [85,90,93]
+
+print("person_dict: ", person)
+
+class Person():
+    
