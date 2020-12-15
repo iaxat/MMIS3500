1da5f6e8f27cb32bf25c4ddd55673f6013ad6cf8
Kumar Akshat <55215422+iaxat@users.noreply.github.com>
2020-12-15 00:24:58 -0700

    up

--- activities/week11.py
+++ activities/week11.py
@@ -1,2 +1,12 @@
 # Week 11
 
+
+
+# Content Videos
+# Files Section 9.3
+file1 = open("output1.txt",'w')
+file1.write('hello\n')
+file1.write('goodbye\n')
+
+with open('output2.txt','w') as file2:
+    file2.write('hello goodbye')
