a5ea915655dfb2f62a3bbf0bdfebf7c6a9d9e372
Kumar Akshat <55215422+iaxat@users.noreply.github.com>
2020-12-15 15:43:29 -0700

    up

--- final_project/final_project.py
+++ final_project/final_project.py
@@ -5,4 +5,11 @@
 # The project is based on Stock Market
 
 # Pre-requisites of the Project
+# 1. Getting Data from web JSON API
+# 2. Storing data in CSV files
+# 3. Auto-update
+# 4. Three Analysis on the data
+# 5. Results inside results.json
+
+
 
