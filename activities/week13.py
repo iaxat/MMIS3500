6aad0d5df44477328922f89cfa05fbd2cc18f029
Kumar Akshat <55215422+iaxat@users.noreply.github.com>
2020-12-15 23:39:03 -0700

    up

--- activities/week13.py
+++ activities/week13.py
@@ -9,3 +9,11 @@ url = 'http://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + \
 
 print('The json process')
 
+request = requests.get(url)
+print(request.text)
+
+rqst_dict = json.loads(request.text)
+print(rqst_dict)
+
+
+json.dump(rqst_dict,open(ticker+'.csv', 'w'))
