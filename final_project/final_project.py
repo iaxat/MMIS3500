78cdbf818378cb682e3a27471a8f62581abea0c5
Kumar Akshat <55215422+iaxat@users.noreply.github.com>
2020-12-16 13:38:48 -0700

    up

--- final_project/final_project.py
+++ final_project/final_project.py
@@ -82,14 +82,14 @@ def meanReversionStrategy(prices, file):
                               prices[i-3] + prices[i-4] + prices[i-5]) / 5
             # print("The Moving Average for last 5 days is", moving_average)
 
-            if (current_price < 0.95*moving_average) and buy == 0:
+            if (current_price > 0.95*moving_average) and buy == 0:
                 buy = current_price
                 print("Buying the Stock", buy)
                 if first_buy == 0:
                     first_buy = buy
                     print("The first buy is at: ", first_buy)
 
-            elif (current_price > 1.05*moving_average) and buy != 0:
+            elif (current_price < 1.05*moving_average) and buy != 0:
                 print("Selling stock at: ", current_price)
                 iterative_profit = current_price - buy
                 buy = 0
@@ -164,6 +164,58 @@ def simpleMovingAverage(prices, file):
 
     results_dict[file] = {
         'total profit': total_profit, 'profit percent': final_profit_percent}
+
+
+def bb(prices, file):
+    add = 0  # variable for adding total
+    counter = 0  # counter to understand how many counts are there
+    buy = 0
+    iterative_profit = 0
+    total_profit = 0
+    first_buy = 0
+
+    # Getting back to Moving Average
+    i = 0
+    for price in prices:
+        add += price
+        counter += 1
+        if i >= 5:
+            current_price = price
+            moving_average = (prices[i-1] + prices[i-2] +
+                              prices[i-3] + prices[i-4] + prices[i-5]) / 5
+            # print("The Moving Average for last 5 days is", moving_average)
+
+            if (current_price < 0.95*moving_average) and buy == 0:
+                buy = current_price
+                print("Buying the Stock", buy)
+                if first_buy == 0:
+                    first_buy = buy
+                    print("The first buy is at: ", first_buy)
+
+            elif (current_price > 1.05*moving_average) and buy != 0:
+                print("Selling stock at: ", current_price)
+                iterative_profit = current_price - buy
+                buy = 0
+                print("This trade Profit is: ", iterative_profit)
+                total_profit += iterative_profit
+                print("")
+
+        i += 1  # Iteration changes the loop process
+
+    # Now processing the profits
+    print("-----------------------MEAN REVERSION total profits earned from the first buy----------------------")
+    final_profit_percent = (total_profit/first_buy) * 100
+    print("")
+    print("The total profit percentage is: ", final_profit_percent)
+    print("")
+
+    results_dict[file] = {
+        'total profit': total_profit, 'profit percent': final_profit_percent}
+
+    # Unrelated but was in the class video so added
+    total_avg = add/counter
+    print("Total Average for price for the whole list is: ", total_avg)
+
 
 
 # Program Running
