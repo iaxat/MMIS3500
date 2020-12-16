ec152aae000b39432c507c2f7909eaf7e30b8a33
Kumar Akshat <55215422+iaxat@users.noreply.github.com>
2020-12-16 01:44:59 -0700

    up

--- final_project/final_project.py
+++ final_project/final_project.py
@@ -20,8 +20,8 @@ import time
 # requests imported to handle API key
 # wait for 1 sec
 
-def web_json():
-    ticker = 'AAPL'
+def web_json(ticker):
+    # ticker = 'AAPL'
     url = 'http://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + \
         ticker+'&outputsize=full&apikey=3D9FOUWO02NOZH93'
     req = requests.get(url)
@@ -29,14 +29,14 @@ def web_json():
 
     req_dct = json.loads(req.text)
     json.dump(req_dct, open(ticker+".json","w"))
-web_json()
 
-def processing_json():
-    ticker = 'AAPL'
-    url = 'http://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + \
-        ticker+'&outputsize=full&apikey=NG9C9EPVYBMQT0C8'
-    req = requests.get(url)
-    req_dictonary = json.loads(req.text)
+
+    # def processing_json():
+    #     ticker = 'AAPL'
+    #     url = 'http://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + \
+    #         ticker+'&outputsize=full&apikey=NG9C9EPVYBMQT0C8'
+    #     req = requests.get(url)
+    #     req_dictonary = json.loads(req.text)
 
     key1 = "Time Series (Daily)"
     # date all  of themn
@@ -45,11 +45,18 @@ def processing_json():
     fil = open(ticker+".csv", "w")
     fil.write("Date,price\n")
 
-    for date in req_dictonary[key1]:
-        # print(date + "," + req_dictonary[key1][date][key2])
-        fil.write(date + "," + req_dictonary[key1][date][key2]+"\n")
+    for date in req_dct[key1]:
+        # print(date + "," + req_dct[key1][date][key2])
+        fil.write(date + "," + req_dct[key1][date][key2]+"\n")
     fil.close()
-processing_json()
+
+
+tickers = ['AAPL', 'CSCO', 'FB', 'GOOGL',
+                 'JPM', 'MSFT']
+web_json()
+
+
+# processing_json()
 # def data_extraction():
 #     tickers = ['AAPL', 'CSCO', 'FB', 'GOOGL',
 #                 'JPM', 'MSFT']
