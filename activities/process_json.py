868bb94155f41aea54e5f0e789c0bc1e0ae380f8
Kumar Akshat <55215422+iaxat@users.noreply.github.com>
2020-12-16 00:08:13 -0700

    up

--- activities/process_json.py
+++ activities/process_json.py
@@ -11,6 +11,8 @@ key1 = "Time Series (Daily)"
 # date all  of themn
 key2 = "4. close"
 
+fil = open(ticker+".csv","w")
+fil.write("Date,price\n")
+
 for date in rqst_dictonary[key1]:
     print(rqst_dictonary[key1][date][key2])
-    
