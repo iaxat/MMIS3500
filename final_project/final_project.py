cb68ae39dae3871f82894b11dcd2732e3561d9b9
Kumar Akshat <55215422+iaxat@users.noreply.github.com>
2020-12-16 21:23:43 -0700

    up

--- final_project/final_project.py
+++ final_project/final_project.py
@@ -9,32 +9,35 @@
 # 4. Three Analysis on the data
 # 5. Results inside results.json
 
-# API Key = NG9C9EPVYBMQT0C8
-
-# Library imports
-import json
-import requests
-import time
 from os import path
+import time
+import requests
+import json
+API_KEY = 'NG9C9EPVYBMQT0C8'
+# Library imports
 # json import to handle the json files
 # requests imported to handle API key
 # wait for 1 sec
 
+mean_reversion_dict = {}
+simple_average_dict = {}
+bb_dict = {}
+
 
 def append(ticker):
     url = 'http://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + \
-        ticker+'&outputsize=full&apikey=NG9C9EPVYBMQT0C8'
+        ticker+'&outputsize=full&apikey=' + API_KEY
     req = requests.get(url)
     time.sleep(13)
 
     req_dict = json.loads(req.text)
-
     print(req_dict.keys())
 
-    key1 = "Time Series (Daily)"  # dictionary with all prices by date
+    key1 = 'Time Series (Daily)'  # dictionary with all prices by date
     key2 = '4. close'
 
-    csv_file = open(ticker + ".csv", "r")
+    csv_file = open("/home/ubuntu/environment/final_project/" +
+                    ticker + ".csv", "r")
     lines = csv_file.readlines()
     last_date = lines[-1].split(",")[0]
 
@@ -46,7 +49,8 @@ def append(ticker):
         new_lines.append(date + "," + req_dict[key1][date][key2]+"\n")
 
     new_lines = new_lines[::-1]
-    csv_file = open(ticker + ".csv", "a")  # opening the file to append data
+    csv_file = open("/home/ubuntu/environment/final_project/" +
+                    ticker + ".csv", "a")  # opening the file to append data
     csv_file.writelines(new_lines)  # appending new data
     csv_file.close()
 
@@ -57,7 +61,7 @@ def append(ticker):
 def process_json(ticker):
 
     url = 'http://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + \
-        ticker+'&outputsize=full&apikey=NG9C9EPVYBMQT0C8'
+        ticker+'&outputsize=full&apikey=' + API_KEY
     req = requests.get(url)
     time.sleep(13)
 
@@ -65,10 +69,11 @@ def process_json(ticker):
 
     print(req_dict.keys())
 
-    key1 = "Time Series (Daily)"  # dictionary with all prices by date
+    key1 = 'Time Series (Daily)'  # dictionary with all prices by date
     key2 = '4. close'
 
-    csv_file = open(ticker + ".csv", "w")
+    csv_file = open("/home/ubuntu/environment/final_project/" +
+                    ticker + ".csv", "w")
     csv_file.write("Date,AAPL\n")
     write_lines = []
     for date in req_dict[key1]:
@@ -88,7 +93,6 @@ def meanReversionStrategy(prices, file):
     print("")
     print("\t !!!!!Starting Mean Reversion Strategy for!!!!!", file)
     print("")
-    results_dict = {}
     buy = 0
     iterative_profit = 0
     total_profit = 0
@@ -138,10 +142,10 @@ def meanReversionStrategy(prices, file):
     print("")
     print("-----------------------------------------------------------------------------------------------------")
 
-    results_dict[file] = {
-        'total profit': total_profit, 'profit percent': final_profit_percent, 'strategy': "Mean Reversion"}
-    print(results_dict)
-    return results_dict
+    mean_reversion_dict[file] = {
+        'total profit': total_profit,
+        'profit percent': final_profit_percent}
+    return total_profit, final_profit_percent
 
 # //////////////////////////////////////////////////
 # Function for simple moving average
@@ -151,7 +155,6 @@ def simpleMovingAverage(prices, file):
     print("")
     print("\t !!!!!Starting Simple Moving Average for!!!!!", file)
     print("")
-    results_dict = {}
     buy = 0
     iterative_profit = 0
     total_profit = 0
@@ -201,10 +204,10 @@ def simpleMovingAverage(prices, file):
     print("")
     print("-----------------------------------------------------------------------------------------------------")
 
-    results_dict[file] = {
-        'total profit': total_profit, 'profit percent': final_profit_percent, 'strategy': "Simple Moving Average"}
-    print(results_dict)
-    return results_dict
+    simple_average_dict[file] = {
+        'total profit': total_profit,
+        'profit percent': final_profit_percent}
+    return total_profit, final_profit_percent
 
 # ///////////////////////////////////////////////////////////
 
@@ -213,7 +216,6 @@ def bb(prices, file):
     print("")
     print("\t !!!!!!Starting Bollinger Bands Strategy for!!!!!!", file)
     print("")
-    results_dict = {}
     buy = 0
     iterative_profit = 0
     total_profit = 0
@@ -263,10 +265,10 @@ def bb(prices, file):
     print("")
     print("-----------------------------------------------------------------------------------------------------")
 
-    results_dict[file] = {
-        'total profit': total_profit, 'profit percent': final_profit_percent, 'strategy': "Bollinger Bands"}
-    print(results_dict)
-    return results_dict
+    bb_dict[file] = {
+        'total profit': total_profit,
+        'profit percent': final_profit_percent}
+    return total_profit, final_profit_percent
 
 
 # /////////////////////////////////////////////////
@@ -275,6 +277,7 @@ def results():
     high_returns = 0
     high_returns_ticker = ""
     high_returns_strategy = ""
+    high_returns_percent = 0
     print("")
     print("")
     print("!!!The final project API requests are at an interval of 13 seconds each!!!")
@@ -282,21 +285,54 @@ def results():
     tickers = ['AAPL', 'CSCO', 'FB', 'GOOGL',
                'JPM', 'MSFT', 'TMUS', 'TSLA', 'TTM', 'XOM']
     for ticker in tickers:
-        if path.isfile(ticker + ".csv"):
-            append(ticker)
-        else:
-            process_json(ticker)
+        # if path.isfile(ticker + ".csv"):
+        #     append(ticker)
+        # else:
+        #     process_json(ticker)
 
         file = open(ticker+".csv")
         lines = file.readlines()[1:]
         prices = [float(line.split(",")[1]) for line in lines]
-        
-        # Mean Reversion Strategy calling function
-        meanReversionStrategy(prices, ticker+".csv")
-        # Simple Moving Strategy calling function
-        simpleMovingAverage(prices, ticker+".csv")
-        # Bollinger Bands Strategy calling function
-        bb(prices, ticker+".csv")  
+
+        total_profit, final_profit_percent = simpleMovingAverage(
+            prices, "/home/ubuntu/environment/final_project/"+ticker+".csv")
+        if total_profit > high_returns:
+            high_returns = total_profit
+            high_returns_ticker = ticker
+            high_returns_percent = final_profit_percent
+            high_returns_strategy = "Simple Moving Average"
+
+        total_profit, final_profit_percent = meanReversionStrategy(
+            prices, "/home/ubuntu/environment/final_project/"+ticker+".csv")
+        if total_profit > high_returns:
+            high_returns = total_profit
+            high_returns_ticker = ticker
+            high_returns_percent = final_profit_percent
+            high_returns_strategy = "Mean Reversion"
+
+        total_profit, final_profit_percent = bb(
+            prices, "/home/ubuntu/environment/final_project/"+ticker+".csv")
+        if total_profit > high_returns:
+            high_returns = total_profit
+            high_returns_ticker = ticker
+            high_returns_percent = final_profit_percent
+            high_returns_strategy = "Simple Moving Average"
+
+    final_result["simpleMovingAverage"] = simple_average_dict
+    final_result["meanReversionStrategy"] = mean_reversion_dict
+    final_result["bb"] = bb_dict
+
+    final_result["MAX PROFIT"] = {"ticker": high_returns_ticker,
+                                  "strategy": high_returns_strategy,
+                                  "returns": high_returns,
+                                  "percent": high_returns_percent}
+
+    print("")
+
+    print("")
+
+    print("")
+    print(final_result)
 
 
 results()
